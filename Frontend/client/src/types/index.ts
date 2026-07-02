07c7542a88756dd885d321b5e975acd6926f81ea
Mukul Kumar Singh <mukulkms.iimtgn@gmail.com>
2026-07-02 22:17:49 +0530

    Update index.ts

--- Frontend/client/src/types/index.ts
+++ Frontend/client/src/types/index.ts
@@ -14,7 +14,6 @@ export interface DashboardStats {
   paidCount: number;
   totalOutstanding: number;
   totalCollected: number; 
-  totalCollected: number;
   collectedThisMonth: number;
   weeklyDue: Bill[];
   recentBills: Bill[];
